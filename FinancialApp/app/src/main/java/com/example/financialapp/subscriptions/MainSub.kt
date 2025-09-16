97e25742257f27bf92bf52cca85c1927730f240c
Ronny <qvz3326@autuni.ac.nz>
2025-09-16 23:13:20 +1200

    test

---
+++ FinancialApp/app/src/main/java/com/example/financialapp/subscriptions/MainSub.kt
@@ -0,0 +1,29 @@
+package com.example.financialapp.subscriptions
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+object Routes {
+    const val HOME = "home"
+
+}
+
+@Composable
+fun MainSub() {
+    val navController = rememberNavController()
+
+    Surface(color = MaterialTheme.colorScheme.background) {
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") {
+                HomeScreen(onOpenSubs = {
+                    navController.navigate("subs")
+                })
+            }
+        }
+    }
+
+}
