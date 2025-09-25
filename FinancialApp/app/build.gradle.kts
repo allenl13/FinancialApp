fc409e0356c05994f75a761107576742765edca0
Tristan Gavina <100380965+TristanGavina@users.noreply.github.com>
2025-09-25 17:29:44 +1200

    gms fixed

--- FinancialApp/app/build.gradle.kts
+++ FinancialApp/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.google.services)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 ksp {
